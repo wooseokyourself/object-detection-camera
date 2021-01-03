c689eb2a59cd493f74db355968ea08cc266674a9
wooseokyourself <21500740@handong.edu>
2021-01-03 20:36:46 +0900

    restful request modularization

--- app.py
+++ app.py
@@ -1,55 +1,22 @@
 import subprocess
-import requests, json
-import time
 from datetime import datetime
 import random # for test
 from packages.API.CATM1 import CATM1
 from packages.API.NRF import NRF
+from packages.API.WEB import WEB
 
-API_ENDPOINT = "http://ino-on.umilevx.com/api/devices/events"
 TIMESTAMP = datetime.now().strftime("%Y-%m-%d_%H:%M:%S")
 IMAGEFILE = TIMESTAMP + ".jpg"
-
 detector = "./build/detector model/yolov4-custom_best.weights model/yolov4-custom.cfg model/classes.names results/" + IMAGEFILE + " 0.4 0.5 416"
 
-taskModePin = 15    # Task Mode Signal Pin (input)
-rpiOffPin = 29      # Work Completion Signal Pin (output)
-ltePwrPin = 21      # CAT.M1 Power Pin (output) --> pin initialized in CZCATM1 package
-lteStatPin = 23     # CAT.M1 Status Pin (input) --> pin initialized in CZCATM1 package
+taskModePin = 15    # NRF - Task Mode Signal Pin (input)
+rpiOffPin = 29      # NRF - Work Completion Signal Pin (output)
+ltePwrPin = 21      # CAT.M1 Power Pin (output)
+lteStatPin = 23     # CAT.M1 Status Pin (input)
 
 lte = CATM1(serialPort='/dev/ttyS0', baudrate=115200, pwrPinNum=ltePwrPin, statPinNum=lteStatPin)
 nrf = NRF(taskPinNum=taskModePin, offPinNum=rpiOffPin)
-
-def post (event, rssi, battery):
-    data = {}
-    if event == False:
-        data = {"time":TIMESTAMP, "event":0, "rssi":rssi, "battery":battery}
-    else:
-        data = {"time":TIMESTAMP, "event":1, "rssi":rssi, "battery":battery, "filename":IMAGEFILE, "files":"@results/"+IMAGEFILE}
-    return requests.post(API_ENDPOINT, json=data)
-
-# Snapshot, inference, request
-def mainTask (rssi, battery):
-    process = subprocess.run(detector, capture_output=True, shell=True)
-    exitCode = process.returncode
-    response = ""
-    if exitCode == 0:
-        print("No event")
-        response = post(event=False, rssi, battery)
-    elif exitCode == 1:
-        print("Event detected")
-        response = post(event=True, rssi, battery)
-    elif exitCode == 2:
-        print("Camera connection failed")
-    elif exitCode == 3:
-        print("Writing output image failed")
-    elif exitCode == 4:
-        print("Load model failed")
-    elif exitCode == 5:
-        print("Inference failed")
-    else:
-        print("Invalid status")
-    print(response.status_code, ":", response.text)
+web = WEB(url="http://ino-on.umilevx.com/api/devices/events")
 
 def adminMode ():
     """
@@ -68,7 +35,29 @@ def adminMode ():
 def basicMode ():
     lte.pwrOnModem() # LTE power on
     rssi, battery = lte.getRSSI(), random.randrange(1, 100) # 배터리 부분 구현해야함
-    mainTask(rssi, battery) # Task
+
+    process = subprocess.run(detector, capture_output=True, shell=True)
+    exitCode = process.returncode
+    
+    if exitCode == 0:
+        print("No event")
+        resCode, resText = post(TIMESTAMP, event=False, rssi, battery)
+        print(resCode, ":", resText)
+    elif exitCode == 1:
+        print("Event detected")
+        resCode, resText = post(TIMESTAMP, event=False, rssi, battery, IMAGEFILE)
+        print(resCode, ":", resText)
+    elif exitCode == 2:
+        print("Camera connection failed")
+    elif exitCode == 3:
+        print("Writing output image failed")
+    elif exitCode == 4:
+        print("Load model failed")
+    elif exitCode == 5:
+        print("Inference failed")
+    else:
+        print("Invalid status")
+    
     lte.pwrOffModem() # LTE power off
     nrf.pwrOffPi()
 
---
+++ packages/API/WEB.py
@@ -0,0 +1,14 @@
+import requests, json
+
+class WEB:
+    def __init__(self, url):
+        self.endPoint = url
+        
+    def post(time, event, rssi, battery, imagefile=None):
+        data = {}
+        if event == False:
+            data = {"time":time, "event":0, "rssi":rssi, "battery":battery}
+        else:
+            data = {"time":time, "event":1, "rssi":rssi, "battery":battery, "filename":imagefile, "files":"@results/"+imagefile}
+        response = requests.post(self.endPoint, json=data)
+        return response.status_code, response.text
