69a4097b29c015531243b8415153e6852851af6d
wooseokyourself <21500740@handong.edu>
2021-01-04 11:13:07 +0900

    import

--- test/cam-yolo.py
+++ test/cam-yolo.py
@@ -1,6 +1,5 @@
-import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append("../")
 import subprocess
 import argparse
 
--- test/catm1.py
+++ test/catm1.py
@@ -1,6 +1,5 @@
-import os
 import sys
-sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+sys.path.append("../")
 from packages.API.CATM1 import CATM1
 
 ltePwrPin = 21      # CAT.M1 Power Pin (output)
