71337ccbde49c702c1222f5af4096ca65ca8644d
wooseokyourself <21500740@handong.edu>
2021-01-06 15:56:12 +0900

    Update WEB.py

--- packages/API/WEB.py
+++ packages/API/WEB.py
@@ -1,15 +1,5 @@
 import requests, json
-import packages.Define as Define
-
-class WEB:
-    def __init__(self, url):
-        self.endPoint = url
         
-    def post(self, time, event, rssi, battery, imagefile=None):
-        data = {}
-        if event == False:
-            data = {"time":time, "event":str(0), "rssi":rssi, "battery":str(battery)}
-        else:
-            data = {"time":time, "event":str(1), "rssi":rssi, "battery":str(battery), "filename":imagefile, "files":"@results/"+imagefile}
-        response = requests.post(self.endPoint, json=data)
-        return response.status_code, response.text
+def post(url, data):
+    response = requests.post(url, json=data)
+    return response.status_code, response.text
