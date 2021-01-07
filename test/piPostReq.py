44454dc2ad70087cf352ea9498750abf18a5c3cc
wooseokyourself <21500740@handong.edu>
2021-01-07 12:31:31 +0900

    Update piPostReq.py

--- test/piPostReq.py
+++ test/piPostReq.py
@@ -5,4 +5,4 @@ URL="http://ino-on.umilevx.com/api/devices/events/ino-on-0000"
 data = {"time":"0000-00-00_00:00:00", "event":"0", "rssi":"99", "battery":"0"}
 
 response = requests.post(URL, files=data)
-pprint(response.json()['headers'])
+pprint(response.headers)
