e5ab2cec531189499a90ed2bd20deda82f371ff8
wooseokyourself <21500740@handong.edu>
2021-01-07 15:37:28 +0900

    Update piPostReq.py

--- test/piPostReq.py
+++ test/piPostReq.py
@@ -2,6 +2,6 @@ import requests, json
 from pprint import pprint
 
 URL="http://ino-on.umilevx.com/api/devices/events/ino-on-0000"
-data = {"time":"0000-00-00_00:00:00", "event":"0", "rssi":"99", "battery":"0"}
+data = {"time":(None, "0000-00-00_00:00:00"), "event":(None, "0"), "rssi":(None, "99"), "battery":(None, "0")}
 response = requests.post(URL, files=data)
 pprint(response)
