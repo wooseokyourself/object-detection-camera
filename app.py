066c0532da269266d2457b02b4a58531c8ab6604
wooseokyourself <21500740@handong.edu>
2021-01-11 11:44:45 +0900

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ os.chdir(pathlib.Path(__file__).parent.absolute())
 from packages.Define import *
 import argparse
 import subprocess
-import ifcfg
 import requests
 import json
 from datetime import datetime
