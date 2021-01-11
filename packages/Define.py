75e9b2aa1cd01a90d84e9f2a9b3de92bb6cde2c5
wooseokyourself <21500740@handong.edu>
2021-01-11 16:42:34 +0900

    Update Define.py

--- packages/Define.py
+++ packages/Define.py
@@ -7,5 +7,3 @@ MODEM_PWR_PIN = 17       # CAT.M1 Power Pin (output)
 MODEM_STAT_PIN = 27     # CAT.M1 Status Pin (input)
 
 MODEM_SER_PORT = "/dev/ttyS0"
-
-URL="http://ino-on.umilevx.com/api/devices/events/ino-on-0000"
