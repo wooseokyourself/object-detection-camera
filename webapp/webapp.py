6f30d6771003dc0449016e66f3556a1d0f56ef10
wooseokyourself <21500740@handong.edu>
2021-01-12 17:08:02 +0900

    webapp

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -1,75 +1,62 @@
-from imutils.video import VideoStream
-from flask import Response
 from flask import Flask
+from flask import Response
 from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import request
+from flask import session
+from flask import abort
+from flask import flash
 import threading
 import argparse
 import datetime
 import imutils
 import time
+from imutils.video import VideoStream
+import imutils.encodings as encodings
 import cv2
 
-# initialize the output frame and a lock used to ensure thread-safe
-# exchanges of the output frames (useful when multiple browsers/tabs
-# are viewing the stream)
 outputFrame = None
 lock = threading.Lock()
-# initialize a flask object
 app = Flask(__name__)
-# initialize the video stream and allow the camera sensor to
-# warmup
 vs = VideoStream(usePiCamera=1, framerate=15).start()
 time.sleep(2.0)
 
-@app.route("/")
+@app.route('/submit', methods=['POST'])
+def submit(confidence_threshold=None, nms_threshold=None, resize=None):
+    if request.method == 'POST':
+        ''' 아래 값들을 config.json 에 넣는 코드 구현해야함 '''
+        print(request.form['confidence_threshold'])
+        print(request.form['nms_threshold'])
+        print(request.form['resize'])
+    return render_template('index.html')
+
+@app.route('/')
 def index():
-	# return the rendered template
-	return render_template("index.html")
+    return render_template('index.html')
 
 def readFrame(frameCount):
-	# grab global references to the video stream, output frame, and
-	# lock variables
-	global vs, outputFrame, lock
-	# initialize the motion detector and the total number of frames
-	# read thus far
-	total = 0
-
-    # loop over frames from the video stream
-	while True:
-		# read the next frame from the video stream, resize it,
-		# convert the frame to grayscale, and blur it
-		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
-		# grab the current timestamp and draw it on the frame
-		timestamp = datetime.datetime.now()
-		cv2.putText(frame, timestamp.strftime(
+    global vs, outputFrame, lock
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=400)
+        timestamp = datetime.datetime.now()
+        cv2.putText(frame, timestamp.strftime(
 			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
-
-		# acquire the lock, set the output frame, and release the
-		# lock
-		with lock:
-			outputFrame = frame.copy()
-	
-
+        with lock:
+            outputFrame = frame.copy()
+    
 def generate():
-	# grab global references to the output frame and lock variables
-	global outputFrame, lock
-	# loop over frames from the output stream
-	while True:
-		# wait until the lock is acquired
-		with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-			if outputFrame is None:
-				continue
-			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-			# ensure the frame was successfully encoded
-			if not flag:
-				continue
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
+    global outputFrame, lock
+    while True:
+        with lock:
+            if outputFrame is None:
+                continue
+            (flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
+            if not flag:
+                continue
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
 			bytearray(encodedImage) + b'\r\n')
 
 @app.route("/videoFeed")
@@ -79,7 +66,6 @@ def videoFeed():
 	return Response(generate(),
 		mimetype = "multipart/x-mixed-replace; boundary=frame")
 
-# check to see if this is the main thread of execution
 if __name__ == '__main__':
 	# construct the argument parser and parse command line arguments
 	ap = argparse.ArgumentParser()
