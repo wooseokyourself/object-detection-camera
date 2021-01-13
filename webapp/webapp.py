4267f72efbf3fa9602040cc5379e5c54e163864b
wooseokyourself <21500740@handong.edu>
2021-01-13 11:24:11 +0900

    Update webapp.py

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -19,7 +19,7 @@ import cv2
 outputFrame = None
 lock = threading.Lock()
 app = Flask(__name__)
-vs = VideoStream(usePiCamera=1, framerate=15).start()
+vs = VideoStream(usePiCamera=1, framerate=1).start()
 time.sleep(2.0)
 
 @app.route('/submit', methods=['POST'])
