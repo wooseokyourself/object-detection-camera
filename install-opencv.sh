c5a5cad08642000e85d9276f49d10e60e321ad91
wooseokyourself <21500740@handong.edu>
2021-01-14 11:21:28 +0900

    without opencv-contrib

--- install-opencv.sh
+++ install-opencv.sh
@@ -16,19 +16,19 @@ sudo apt-get install protobuf-compiler
 
 
 wget -O ~/opencv.zip https://github.com/opencv/opencv/archive/4.5.0.zip
-wget -O ~/opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.0.zip
+# wget -O ~/opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/4.5.0.zip
 
 unzip ~/opencv.zip
-unzip ~/opencv_contrib.zip
+# unzip ~/opencv_contrib.zip
 
 mv ~/opencv-4.5.0 ~/opencv
-mv ~/opencv_contrib-4.5.0 ~/opencv_contrib
+# mv ~/opencv_contrib-4.5.0 ~/opencv_contrib
 
 mkdir ~/opencv/build
 cd ~/opencv/build
 cmake -D CMAKE_BUILD_TYPE=RELEASE \
         -D CMAKE_INSTALL_PREFIX=/usr/local \
-        -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
+#         -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib/modules \
         -D ENABLE_NEON=ON \
         -D WITH_FFMPEG=ON \
         -D WITH_TBB=ON \
