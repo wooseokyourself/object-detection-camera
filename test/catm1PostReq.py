b997cee6388a08da0e97b9a75f85e93ccef9012a
wooseokyourself <21500740@handong.edu>
2021-01-18 11:41:58 +0900

    remove API

--- test/catm1PostReq.py
+++ test/catm1PostReq.py
@@ -2,7 +2,7 @@ import sys
 sys.path.append("../")
 import json
 from packages.Define import *
-from packages.API.CATM1 import CATM1
+from packages.CATM1 import CATM1
 import RPi.GPIO as GPIO
 
 node = CATM1(serialPort=MODEM_SER_PORT, baudrate=115200, pwrPinNum=MODEM_PWR_PIN, statPinNum=MODEM_STAT_PIN)
--- test/catm1PwrOn.py
+++ test/catm1PwrOn.py
@@ -1,7 +1,7 @@
 import sys 
 sys.path.append("../")
 from packages.Define import *
-from packages.API.CATM1 import CATM1
+from packages.CATM1 import CATM1
 
 node = CATM1(serialPort=MODEM_SER_PORT, baudrate=115200, pwrPinNum=MODEM_PWR_PIN, statPinNum=MODEM_STAT_PIN)
 node.pwrOnModem()
