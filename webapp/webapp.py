0fa7ba3540b5d4528f12eb6f66ddde7b6e2b8b33
wooseokyourself <21500740@handong.edu>
2021-01-26 13:57:20 +0900

    Update webapp.py

--- webapp/webapp.py
+++ webapp/webapp.py
@@ -103,8 +103,6 @@ if __name__ == '__main__':
 		help="ip address of the device")
 	ap.add_argument("-o", "--port", type=int, required=True,
 		help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-f", "--frame-count", type=int, default=32,
-		help="# of frames used to construct the background model")
 	args = vars(ap.parse_args())
 	# start a thread that will perform motion detection
 	t = threading.Thread(target=readFrame, args=(
