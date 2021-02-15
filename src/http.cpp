806bb22096242e5d2e85ecd299d79b081dc5eafd
wooseokyourself <21500740@handong.edu>
2021-02-15 16:29:44 +0900

    add post for preview picture

--- src/http.cpp
+++ src/http.cpp
@@ -1,13 +1,80 @@
 #include "../include/http.hpp"
 
+/// @brief 관리자모드에서 사용되는 POST
 bool
 http::post (const string& url, 
-            const string& time,
-            const int& rssi,
-            const int& battery, 
-            const string& filename, 
-            const string& filepath, 
-            const long& timeoutsecs) {
+                 const strimg& time, 
+                 const string& filename, 
+                 const string& filepath, 
+                 const long& timeoutsecs) {
+
+    struct curl_httppost* formpost = NULL;
+    struct curl_httppost* lastptr = NULL;
+    struct curl_slist* headerlist = NULL;
+    static const char buf[] = "Expect:";
+
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    curl_formadd(&formpost,
+                 &lastptr,
+                 CURLFORM_COPYNAME, "time",
+                 CURLFORM_COPYCONTENTS, time.c_str(),
+                 CURLFORM_END);
+
+    curl_formadd(&formpost,
+                 &lastptr,
+                 CURLFORM_COPYNAME, "filename",
+                 CURLFORM_COPYCONTENTS, filename.c_str(),
+                 CURLFORM_END);
+
+    // from file
+    curl_formadd(&formpost,
+                 &lastptr,
+                 CURLFORM_COPYNAME, "files",
+                 CURLFORM_FILE, filepath.c_str(),
+                 CURLFORM_END);
+                 
+    curl_formadd(&formpost,
+                 &lastptr,
+                 CURLFORM_COPYNAME, "submit",
+                 CURLFORM_COPYCONTENTS, "send",
+                 CURLFORM_END);
+
+    CURL* curl = curl_easy_init();
+
+    headerlist = curl_slist_append(headerlist, buf);
+    if(curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_HTTPPOST, formpost);
+        curl_easy_setopt(curl, CURLOPT_TIMEOUT, timeoutsecs);
+
+        CURLcode res = curl_easy_perform(curl);
+        while (res != CURLE_OK && res != CURLE_OPERATION_TIMEDOUT) {
+            res = curl_easy_perform(curl);
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+            delay(2500);
+        }
+        curl_easy_cleanup(curl);
+        curl_formfree(formpost);
+        curl_slist_free_all (headerlist);
+        if (res != CURLE_OPERATION_TIMEDOUT)
+            return true;
+        else
+            return false;
+    }
+    else
+        return false;
+}
+
+/// @brief 일반모드에서 이벤트가 감지되었을때 사용되는 POST
+bool
+http::post (const string& url, 
+                  const string& time,
+                  const int& rssi,
+                  const int& battery, 
+                  const string& filename, 
+                  const string& filepath, 
+                  const long& timeoutsecs) {
 
     struct curl_httppost* formpost = NULL;
     struct curl_httppost* lastptr = NULL;
@@ -53,14 +120,6 @@ http::post (const string& url,
                  CURLFORM_FILE, filepath.c_str(),
                  CURLFORM_END);
 
-/*  // from buffer = 이거 구현되면 결과사진 파이에 저장할 필요 없음
-    curl_formadd(&formpost,
-                &lastptr,
-                CURLFORM_COPYNAME, "files",
-                CURLFORM_BUFFERPTR, imgBuffPtr, 
-                CURLFORM_BUFFERLENGTH, imgBuffSize,
-                CURLFORM_END);
-*/ 
     curl_formadd(&formpost,
                  &lastptr,
                  CURLFORM_COPYNAME, "submit",
@@ -93,12 +152,13 @@ http::post (const string& url,
         return false;
 }
 
+/// @brief 일반모드에서 이벤트가 감지되지 않았을때 사용되는 POST
 bool
 http::post (const string& url, 
-            const string& time,
-            const int& rssi,
-            const int& battery, 
-            const long& timeoutsecs) {
+                  const string& time,
+                  const int& rssi,
+                  const int& battery, 
+                  const long& timeoutsecs) {
 
     struct curl_httppost* formpost = NULL;
     struct curl_httppost* lastptr = NULL;
