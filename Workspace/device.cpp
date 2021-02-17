bb4aeb7e2bfd354c85b02d2855ebd1ba95df0500
wooseokyourself <21500740@handong.edu>
2021-02-17 14:38:53 +0900

    Update device.cpp

--- Workspace/device.cpp
+++ Workspace/device.cpp
@@ -250,7 +250,7 @@ void
 atcmd::__sendATcmd (const int fd, const char* cmd, const size_t len) {
     std::cout << "target bytes: " << len << endl;
     std::cout << "Pi) Send AT cmd: " << cmd << std::endl;
-    std::cout << "written bytes: " << write(fd, cmd, len) << endl;
+    std::cout << "written bytes: " << write(fd, cmd, len) << std::endl;
     delay(500);
 }
 
