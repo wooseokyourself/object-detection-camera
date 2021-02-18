9cb2fd1efa82ec06cc60dda4347a209da7c6688c
wooseokyourself <21500740@handong.edu>
2021-02-18 16:02:36 +0900

    config methods and member vars

--- include/config.hpp
+++ include/config.hpp
@@ -4,6 +4,7 @@
 #include <fstream>
 #include <string>
 #include <jsoncpp/json/json.h>
+#include <jsoncpp/json/writer.h>
 
 #define DETECTING_MODE 0
 #define PREVIEW_MODE 1
@@ -12,10 +13,12 @@ class Config {
 public:
     void readFromJsonFile (const std::string filePath);
     void readFromJsonString (const std::string jsonString);
+    void write (const std::string filePath) const;
     std::string getID () const;
     float getConfThreshold () const;
     float getNmsThreshold () const;
     int getWidth () const;
+    int getIntervalSecs () const;
     bool isPreviewMode () const;
     bool isDetectingMode () const;
 
@@ -28,6 +31,7 @@ private:
     float NMS_THRESHOLD;
     int WIDTH;
     int MODE;
+    int INTERVAL_SECS;
 };
 
 #endif
