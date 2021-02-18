875aeaa283d024d98158a405b564f12df3d05eab
wooseokyourself <21500740@handong.edu>
2021-02-18 13:39:18 +0900

    Config init

--- include/config.hpp
+++ include/config.hpp
@@ -1,4 +1,33 @@
 #ifndef CONFIG
 #define CONFIG
 
+#include <fstream>
+#include <string>
+#include <jsoncpp/json/json.h>
+
+#define DETECTING_MODE 0
+#define PREVIEW_MODE 1
+
+class Config {
+public:
+    void readFromJsonFile (const std::string filePath);
+    void readFromJsonString (const std::string jsonString);
+    std::string getID () const;
+    float getConfThreshold () const;
+    float getNmsThreshold () const;
+    int getWidth () const;
+    bool isPreviewMode () const;
+    bool isDetectingMode () const;
+
+private:
+    void readJsonObject (Json::Value& root);
+
+private:
+    std::string ID;
+    float CONF_THRESHOLD;
+    float NMS_THRESHOLD;
+    int WIDTH;
+    int MODE;
+};
+
 #endif
--- src/config.cpp
+++ src/config.cpp
@@ -1,1 +1,54 @@
 #include "../include/Config.hpp"
+
+void Config::readFromJsonFile (const std::string filePath) {
+    Json::Value root;
+    Json::Reader reader;
+    std::ifstream jsonFile(filePath, std::ifstream::binary);
+    if (!reader.parse(jsonFile, root)) {
+        std::cerr << "Config: " << "failed to parse" << reader.getFormattedErrorMessages() << std::endl;
+        return;
+    }
+    this->readJsonObject(root);
+}
+
+void Config::readFromJsonString (const std::string jsonString) {
+    Json::Value root;
+    Json::Reader reader;
+    if (!reader.parse(jsonString.c_str(), root)) {
+        std::cerr << "Config: " << "failed to parse" << reader.getFormattedErrorMessages() << std::endl;
+        return;
+    }
+    this->readJsonObject(root);
+}
+
+std::string Config::getID () const {
+    return this->ID;
+}
+
+float Config::getConfThreshold () const {
+    return this->CONF_THRESHOLD;
+}
+
+float Config::getNmsThreshold () const {
+    return this->NMS_THRESHOLD;
+}
+
+int Config::getWidth () const {
+    return this->WIDTH;
+}
+
+bool Config::isPreviewMode () const {
+    return this->MODE == PREVIEW_MODE ? true : false;
+}
+
+bool Config::isDetectingMode () const {
+    return this->MODE == DETECTING_MODE ? true : false;
+}
+
+void Config::readJsonObject (Json::Value& root) {
+    this->ID = root["ID"].asString();
+    this->CONF_THRESHOLD = std::stof(root["CONF_THRESHOLD"].asString());
+    this->NMS_THRESHOLD = std::stof(root["NMS_THRESHOLD"].asString());
+    this->WIDTH = std::stoi(root["WIDTH"].asString());
+    this->MODE = std::stoi(root["MODE"].asString());
+}
