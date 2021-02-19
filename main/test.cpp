7bd32ac0caca9b35253c77d539b3f1ba507a1b2e
wooseokyourself <21500740@handong.edu>
2021-02-19 13:39:22 +0900

    Update test.cpp

--- main/test.cpp
+++ main/test.cpp
@@ -15,8 +15,8 @@ const std::string DETECTING_URI = "api/devices/events/";
 const std::string PREVIEW_URI = "api/manager/device/";
 
 const int RPI_OFF_PIN = 21; // BCM
-const char[] MODEM_PORT = "/dev/ttyS0";
-const char[] NRF_PORT = " ";
+char[] MODEM_PORT = "/dev/ttyS0";
+char[] NRF_PORT = " ";
 
 int main (void) {
     Config config;
