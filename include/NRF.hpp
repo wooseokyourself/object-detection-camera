55bd33b1467afdf2e8482536f6040bdb20d20a39
wooseokyourself <21500740@handong.edu>
2021-02-24 14:45:46 +0900

    NRF init

--- include/NRF.hpp
+++ include/NRF.hpp
@@ -1,4 +1,22 @@
 #ifndef __NRF__
 #define __NRF__
 
+#include <iostream>
+#include <chrono>
+#include <string>
+#include <cstdlib>
+#include <unistd.h>
+#include "Serial.hpp"
+
+class NRF : public Serial {
+public:
+    NRF (); // just leaving now
+    NRF (const char* port, const int baudRate);
+    NRF (const std::string port, const int baudRate);
+    ~NRF ();
+
+    int getBattery () const;
+    void setPowerInterval () const;
+};
+
 #endif
--- src/NRF.cpp
+++ src/NRF.cpp
@@ -1,1 +1,21 @@
 #include "../include/NRF.hpp"
+
+NRF::NRF () { }
+
+NRF::NRF (const char* port, const int baudRate) : Serial(port, baudRate) {
+    
+}
+
+NRF::NRF (const std::string port, const int baudRate) : Serial(port.c_str(), baudRate) {
+
+}
+
+NRF::~NRF () { }
+
+int NRF::getBattery () const {
+    return 100;
+}
+
+void NRF::setPowerInterval () const {
+    // send to nrf
+}
