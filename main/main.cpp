5b2def6ad6cde3d58f1c057ce10e6d6f86ca0aff
wooseokyourself <21500740@handong.edu>
2021-02-24 15:07:33 +0900

    remove preview mode

--- main/main.cpp
+++ main/main.cpp
@@ -10,7 +10,7 @@
 const std::string JSON_PATH = "config/config.json";
 const std::string HOST = "ino-on.umilevx.com";
 const std::string DETECTING_URI = "/api/devices/events/";
-const std::string PREVIEW_URI = "/api/manager/device/";
+// const std::string PREVIEW_URI = "/api/manager/device/";
 
 const int RPI_OFF_PIN = 21; // BCM
 const std::string MODEM_PORT = "/dev/ttyS0";
@@ -31,44 +31,45 @@ int main (void) {
     config.readFromJsonFile(JSON_PATH);
 
     std::string imageBytes;
-    if (config.isDetectingMode()) {
-        const std::string TIMESTAMP = getISOCurrentTimestamp();
-        const std::stirng FILENAME = TIMESTAMP.substr(0, 10) + ".jpg"; 
-        vision.setModel("bin/model/yolov4.weights", "bin/model/yolov4.cfg", "bin/model/classes.names");
-        vision.capture(config.getCaptureWidth()); // picture width (ratio is 4:3)
-        int detectedCount = vision.detect(EXCAVATOR, config.getConfThreshold(), config.getNmsThreshold(), config.getCaptureWidth());
 
-        fields.addField("text/plain", "time", TIMESTAMP);
-        fields.addField("text/plain", "event", detectedCount > 0 ? "1" : "0");
-        fields.addField("text/plain", "rssi", std::to_string(modem.getRssi()));
-        fields.addField("text/plain", "battery", std::to_string(nrf.getBattery()));
-        if (detectedCount > 0 || config.sendPictureAlways()) {
-            vision.resizeFrame(256);
-            vision.writeFrame("results/" + FILENAME);
-            vision.extractImagefileBytes(imageBytes, "results/" + FILENAME);
-            fields.addField("text/plain", "filename", FILENAME);
-            fields.addField("image/jpeg", FILENAME, imageBytes);
-        }
+    // Detecting Mode
+    const std::string TIMESTAMP = getISOCurrentTimestamp();
+    const std::stirng FILENAME = TIMESTAMP.substr(0, 10) + ".jpg"; 
+    vision.setModel("bin/model/yolov4.weights", "bin/model/yolov4.cfg", "bin/model/classes.names");
+    vision.capture(config.getCaptureWidth()); // picture width (ratio is 4:3)
+    const int detectedCount
+        = vision.detect(EXCAVATOR, config.getConfThreshold(), config.getNmsThreshold(), config.getCaptureWidth());
+    fields.addField("text/plain", "time", TIMESTAMP);
+    fields.addField("text/plain", "event", detectedCount > 0 ? "1" : "0");
+    fields.addField("text/plain", "rssi", std::to_string(modem.getRssi()));
+    fields.addField("text/plain", "battery", std::to_string(nrf.getBattery()));
+    if (detectedCount > 0 || config.sendPictureAlways()) {
+        vision.resizeFrame(256);
+        vision.writeFrame("results/" + FILENAME);
+        vision.extractImagefileBytes(imageBytes, "results/" + FILENAME);
+        fields.addField("text/plain", "filename", FILENAME);
+        fields.addField("image/jpeg", FILENAME, imageBytes);
+    }
 
-        std::string response = modem.postMultipart(HOST, DETECTING_URI + config.getID(), fields, 20);
+    std::string response = modem.postMultipart(HOST, DETECTING_URI + config.getID(), fields, 20);
+    config.readFromJsonString(response);
+    nrf.setPowerInterval(config.getIntervalSecs());
+    
+    /* // Preview Mode
+    while (config.isPreviewMode()) {
+        vision.capture(256);
+        vision.writeFrame("results/preview.jpg")
+        vision.extractImagefileBytes(imageBytes, "results/preview.jpg");
+        fields.addField("image/jpeg", "files", imageBytes);
+        std::string response = modem.postMultipart(HOST, PREVIEW_URI + config.getID(), fields, 20);
         config.readFromJsonString(response);
         nrf.setPowerInterval(config.getIntervalSecs());
+        fields.clear();
     }
-    else {
-        while (config.isPreviewMode()) {
-            vision.capture(256);
-            vision.writeFrame("results/preview.jpg")
-            vision.extractImagefileBytes(imageBytes, "results/preview.jpg");
-            fields.addField("image/jpeg", "files", imageBytes);
-            std::string response = modem.postMultipart(HOST, PREVIEW_URI + config.getID(), fields, 20);
-            config.readFromJsonString(response);
-            nrf.setPowerInterval(config.getIntervalSecs());
-            fields.clear();
-        }
-    }
+    */
 
     config.write(JSON_PATH);
 
-    gpio.shutdownRpi();
+    // gpio.shutdownRpi();
     return 0;
 }
