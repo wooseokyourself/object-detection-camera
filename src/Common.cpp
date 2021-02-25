8e6fbd3c742c5cf7f32d07c99f1434d5a1308f49
wooseokyourself <21500740@handong.edu>
2021-02-25 16:35:12 +0900

    comments

--- src/Common.cpp
+++ src/Common.cpp
@@ -1,5 +1,9 @@
 #include "../include/Common.hpp"
 
+/**
+ * @brief Get timestamp as an ISO format.
+ * @return Format is like "2021-01-08T13:41:21.046Z".
+ */
 std::string getISOCurrentTimestamp () {
     return date::format("%FT%TZ", date::floor<std::chrono::milliseconds>(std::chrono::system_clock::now()));
 }
